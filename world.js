0d221d957f3a919298a13ba56f8bdc04fcb6cc31
Nikita Clemetson <nikita.clemetson@mymona.uwi.edu>
2020-11-19 23:50:32 -0500

    Starting Ex 5

--- world.js
+++ world.js
@@ -1,55 +1,9 @@
-/*$(document).ready(function(){
 
-    var lookbtn=$('#lookup');
-    var result=$('#result');
-
-
-    //var url="world.php";
-    //httpR.onreadystatechange=searchC();
-    //httpR.open('POST', url);
-    //httpR.send();
-
-    lookbtn.on('click', function (){//searchC(){
-        var httpR=new XMLHttpRequest();
-        if (httpR.readyState===XMLHttpRequest.DONE){
-            if(httpR.status===200){
-                var resp=httpR.responseText;
-                result.innerHTML=resp;
-                console.log(resp);
-            }
-        }
-    });
-
-$(document).ready(function() {
-  var lookBtn = $('#lookup');
-  var result = $('#result');
-
-  lookBtn.on('click', function(elem) {
-      elem.preventDefault();
-    $.ajax('world.php', {
-      method: 'POST'
-    }).done(function(response) {
-      result.html(response);
-    }).fail(function() {
-      alert('There was a problem with the request.');
-    });
-  });
-});
-
-
-    /*$.ajax("world.php", {
-        type:"POST",
-        data:"action="+resultv;
-        
-    }).done(fucntion(resp){
-        $('#result').html(resp);
-    });*/
-
-//})
 
 window.onload = function() {
 
     var lookBtn = document.querySelector('#lookup');
+    var lookCBtn = document.querySelector('#city');
     var httpReq;
   
     lookBtn.addEventListener('click', function(element) {
@@ -75,6 +29,29 @@ window.onload = function() {
         }
       }
     }
+
+    lookCBtn.addEventListener('click', function(element) {
+      element.preventDefault();
+        
+  
+      httpReq = new XMLHttpRequest();
+  
+      var url = "world.php";
+      httpReq.onreadystatechange = loadResult;
+      httpReq.open('GET', url);
+      httpReq.send();
+    });
+  
+    function loadResult() {
+      if (httpReq.readyState === XMLHttpRequest.DONE) {
+        if (httpReq.status === 200) {
+          var response = httpReq.responseText;
+          var result = document.querySelector('#result');
+          result.innerHTML = response;
+        } else {
+          alert('No result found.');
+        }
+      }
+    }
   
   }
-  
--- world.php
+++ world.php
@@ -3,48 +3,53 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
-//if ( isset( $_GET['submit'] ) ) {
-  $country=$_GET['country'];
-//}
+$country='';
+if (isset($_POST['lookup'])){
+$country=$_POST['country'];}
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%' ");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%' ");
 
-?>
- <html>
-  <style>
-  table, td, th {
-     text-align: center; 
-    vertical-align: middle;
-  border: 3px solid #272727;
-}
-th{
-  font-size:24px;
-}
-table {
-  margin-left: auto;
-  margin-right: auto;
-  width:1000px; 
-  height:40px;
-  border-collapse: collapse;
-}</style>
-    <table >
-    <tr> 
-			  <th> Name </th> 
-			  <th> Continent </th> 
-			  <th> Independence </th> 
-			  <th> Head of State </th> 
-			  
-		</tr> 
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-<?php foreach ($results as $row): ?>
-  <tr><td><?php echo $row['name']; ?></td>
-   <td><?php echo $row['continent'];?></td>
-    <td><?php echo $row['independence_year'] ;?></td>
-    <td> <?php echo $row['head_of_state']; ?></td>
-  </tr>
-<?php endforeach; ?>
-</table>
-</html>
+  ?>
+  <html>
+    <style>
+    table, td, th {
+      text-align: center; 
+      vertical-align: middle;
+    border: 3px solid #272727;
+  }
+  th{
+    font-size:24px;
+  }
+  table {
+    margin-left: auto;
+    margin-right: auto;
+    width:1000px; 
+    height:40px;
+    border-collapse: collapse;
+  }</style>
+      <table >
+      <tr> 
+          <th> Name </th> 
+          <th> Continent </th> 
+          <th> Independence </th> 
+          <th> Head of State </th> 
+          
+      </tr> 
+
+  <?php foreach ($results as $row): ?>
+    <tr><td><?php echo $row['name']; ?></td>
+    <td><?php echo $row['continent'];?></td>
+      <td><?php echo $row['independence_year'] ;?></td>
+      <td> <?php echo $row['head_of_state']; ?></td>
+    </tr>
+  <?php endforeach; ?>
+  </table>
+  </html>
+
+  
+  
+
