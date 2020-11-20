ecf94a4064f7b7d613cf713d2603a2452d83c463
Nikita Clemetson <nikita.clemetson@mymona.uwi.edu>
2020-11-19 20:24:27 -0500

    Ex 3 paetially finished

--- world.js
+++ world.js
@@ -1,1 +1,81 @@
-'use: strict'
+/*$(document).ready(function(){
+
+    var lookbtn=$('#lookup');
+    var result=$('#result');
+
+
+    //var url="world.php";
+    //httpR.onreadystatechange=searchC();
+    //httpR.open('POST', url);
+    //httpR.send();
+
+    lookbtn.on('click', function (){//searchC(){
+        var httpR=new XMLHttpRequest();
+        if (httpR.readyState===XMLHttpRequest.DONE){
+            if(httpR.status===200){
+                var resp=httpR.responseText;
+                result.innerHTML=resp;
+                console.log(resp);
+            }
+        }
+    });
+
+$(document).ready(function() {
+  var lookBtn = $('#lookup');
+  var result = $('#result');
+
+  lookBtn.on('click', function(elem) {
+      elem.preventDefault();
+    $.ajax('world.php', {
+      method: 'POST'
+    }).done(function(response) {
+      result.html(response);
+    }).fail(function() {
+      alert('There was a problem with the request.');
+    });
+  });
+});
+
+
+    /*$.ajax("world.php", {
+        type:"POST",
+        data:"action="+resultv;
+        
+    }).done(fucntion(resp){
+        $('#result').html(resp);
+    });*/
+
+//})
+
+window.onload = function() {
+
+    var lookBtn = document.querySelector('#lookup');
+    var httpRequest;
+  
+    lookBtn.addEventListener('click', function(element) {
+      element.preventDefault();
+        console.log(document.querySelector('#country').value);
+  
+      httpRequest = new XMLHttpRequest();
+  
+      // GET Request
+      var url = "world.php";
+      httpRequest.onreadystatechange = loadResult;
+      httpRequest.open('GET', url);
+      httpRequest.send();
+    });
+  
+    function loadResult() {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          var response = httpRequest.responseText;
+          var result = document.querySelector('#result');
+          result.innerHTML = response;
+        } else {
+          alert('There was a problem with the request.');
+        }
+      }
+    }
+  
+  }
+  
--- world.php
+++ world.php
@@ -3,9 +3,12 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
+//if ( isset( $_GET['submit'] ) ) {
+  $country=$_GET['country'];
+//}
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%' ");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
